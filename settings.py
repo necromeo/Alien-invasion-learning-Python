b83a92a9b6e14293a0b751d87cce3d71dd6f89ab
necromeo <andrewcrazy@gmail.com>
2018-07-03 12:55:27 -0300

    Created rows. Now it's time to make the fleet move.

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ class Settings():
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 640
-        self.screen_height = 480
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230,230,230)
         self.ship_speed_factor = 1.5
 
